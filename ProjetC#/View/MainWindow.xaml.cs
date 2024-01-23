231c385d19dd77a59ed32a675f8ac6eaa1a145fa
Thomasfzr <thfltzr@gmail.com>
2024-01-23 14:54:33 +0100

    Oui

--- ProjetC#/View/MainWindow.xaml.cs
+++ ProjetC#/View/MainWindow.xaml.cs
@@ -1,22 +1,29 @@
-﻿using Game.ViewModel;
+﻿using System.Windows;
 
 namespace Game
 {
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
         }
 
-        private void ButtonPlay_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void JouerButton_Click(object sender, RoutedEventArgs e)
         {
+            // Masquer l'interface 1 et afficher l'interface 2
 
         }
 
-        private void ButtonQuit_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void QuitterButton_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Quitter button clicked!");
+            Close(); // Fermer l'application
+        }
+
+        private void RetourButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Masquer l'interface 2 et afficher l'interface 1
 
         }
     }
