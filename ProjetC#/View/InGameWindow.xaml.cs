975df7f1bba3bc86255d4ba73d1b60dabc7a432f
ThomasFzr <thfltzr@gmail.com>
2024-01-30 17:15:23 +0100

    merge

--- ProjetC#/View/InGameWindow.xaml.cs
+++ ProjetC#/View/InGameWindow.xaml.cs
@@ -1,20 +1,75 @@
 ﻿using Game.Model;
+using System;
+using System.Media;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
 
 namespace Game.View;
 
 public partial class InGameWindow : Window
 {
     private ShopWindow? shopWindow;
+    private string[] imagePaths = { "/photos/1.png", "/photos/2.png", "/photos/3.png", "/photos/3.png" };
+    private int currentImageIndex = 0;
+    private DispatcherTimer timer;
+    private MediaElement backgroundMusic;
+    private SoundPlayer attackSoundPlayer;
 
     public InGameWindow()
     {
         InitializeComponent();
+        InitializeTimer();
         DataContext = GameManager.Instance;
         shopWindow = new ShopWindow(GameManager.Instance.Player);
         PlayerState.Instance.OnPlayerToPlay += PlayerTurn;
 
+
+        Storyboard monsterAnimation = (Storyboard)this.Resources["monsterAnimation"];
+        monsterAnimation.Begin();
+
+        attackSoundPlayer = new SoundPlayer("C:\\Users\\thoma\\source\\repos\\ProjetC#\\ProjetC#\\music\\metal.wav");
+        attackSoundPlayer.Load();
+
+        backgroundMusic = new MediaElement();
+        backgroundMusic.Source = new Uri("D:\\jeu\\ProjetC#\\music\\background.mp3");
+        backgroundMusic.MediaEnded += BackgroundMusic_MediaEnded;
+        backgroundMusic.LoadedBehavior = MediaState.Manual;
+        backgroundMusic.UnloadedBehavior = MediaState.Manual;
+        backgroundMusic.Play();
+        currentImageIndex = 0;
+        timer.Start();
+
+
+    }
+
+
+    private void BackgroundMusic_MediaEnded(object sender, RoutedEventArgs e)
+    {
+        backgroundMusic.Position = TimeSpan.Zero;
+        backgroundMusic.Play();
+    }
+
+    private void InitializeTimer()
+    {
+        timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromSeconds(0.2);
+        timer.Tick += Timer_Tick;
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        animatedImage.Source = new BitmapImage(new Uri(imagePaths[currentImageIndex], UriKind.RelativeOrAbsolute));
+
+        currentImageIndex = (currentImageIndex + 1) % imagePaths.Length;
+
+        if (currentImageIndex == 0)
+        {
+            timer.Stop();
+        }
     }
 
     private void SpellButton_Click(object sender, RoutedEventArgs e)
--- ProjetC#/View/MainWindow.xaml.cs
+++ ProjetC#/View/MainWindow.xaml.cs
@@ -1,18 +1,56 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace Game.View;
 
 public partial class MainWindow : Window
 {
     private InGameWindow? inGameWindow;
+    private MediaPlayer mediaPlayer;
 
     public MainWindow()
     {
         InitializeComponent();
+        InitializeMediaPlayer().Wait();
+        PlayAudio();
+
+        Storyboard monsterAnimation = (Storyboard)this.Resources["monsterAnimation"];
+        monsterAnimation.Begin();
+
+        Storyboard monsterAnimationn = (Storyboard)this.Resources["monsterAnimationn"];
+        monsterAnimationn.Begin();
+    }
+
+    private async Task InitializeMediaPlayer()
+    {
+        mediaPlayer = new MediaPlayer();
+
+        var audioFilePath = "C:\\Users\\thoma\\source\\repos\\ProjetC#\\ProjetC#\\music\\menu.mp3";
+
+        mediaPlayer.Open(new Uri(audioFilePath, UriKind.RelativeOrAbsolute));
+
+        mediaPlayer.Volume = 0.5; 
+
+        mediaPlayer.MediaFailed += MediaPlayer_MediaFailed;
+    }
+
+    private void MediaPlayer_MediaFailed(object sender, ExceptionEventArgs e)
+    {
+        MessageBox.Show($"Échec du chargement du média : {e.ErrorException.Message}");
+    }
+
+
+    private void PlayAudio()
+    {
+        mediaPlayer.Play();
     }
 
     private void JouerButton_Click(object sender, RoutedEventArgs e)
     {
+        mediaPlayer.Stop();
         inGameWindow = new();
         inGameWindow.Show();
         Close();
@@ -21,6 +59,7 @@ public partial class MainWindow : Window
 
     private void QuitterButton_Click(object sender, RoutedEventArgs e)
     {
+        mediaPlayer.Stop();
         Close();
     }
 }
