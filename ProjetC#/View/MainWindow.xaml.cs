39726835968160807d579682b1a381ab0346037f
ThomasFzr <thfltzr@gmail.com>
2024-02-01 12:38:34 +0100

    fix main

--- ProjetC#/View/MainWindow.xaml.cs
+++ ProjetC#/View/MainWindow.xaml.cs
@@ -10,8 +10,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        backgroundVideo.Position = TimeSpan.Zero;
+        backgroundVideo.Play();
     }
-    
+
     private void BackgroundVideo_MediaEnded(object sender, RoutedEventArgs e)
     {
         backgroundVideo.Position = TimeSpan.Zero;
